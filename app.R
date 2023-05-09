b553da5019c08a8706e586cc7d63e4067190d272
zhi-rong <jordan990301@gmail.com>
2023-05-09 15:58:35 +0800

    fix some bug

--- app.R
+++ app.R
@@ -7,7 +7,7 @@ library(FactoMineR)
 library(factoextra)
 
 ui <- fluidPage(
-    titlePanel("Interactive web service of PCA and CA analysis by Shinyapp"),
+    titlePanel("PCA & CA analysis By 資碩工一 111753151 程至榮"),
     sidebarLayout(
         sidebarPanel(
             helpText("choose how many input to do PCA:"),
@@ -16,9 +16,9 @@ ui <- fluidPage(
                 label = "Number of points",
                 min = 6,
                 max = 150,
-                value = 100
+                value = 150
             ),
-            helpText("choose what you want to see on 'PCA Result : Plot'"),
+            helpText("choose which component to show on 'PCA'"),
             selectInput(
                 "x_var",
                 label = "X Variable",
@@ -39,20 +39,48 @@ ui <- fluidPage(
             ),
         ),
         mainPanel(tabsetPanel(
-            tabPanel("PCA",
-                     plotOutput("pca_plot")),
-            tabPanel("CA",
-                     plotOutput("ca_plot")),
             tabPanel(
-                "Raw Data",
+                "PCA",
+                h3("Summary of PCA (log Transformed) "),
+                verbatimTextOutput("pca_summary"),
+                h3("Biplot of PCA (log Transformed)"),
+                plotOutput("pca_biplot"),
+            ),
+            tabPanel("CA",
+                     h3("Biplot of CA"),
+                     plotOutput("ca_biplot")
+            ),
+            tabPanel(
+                "Input Data",
                 h3("Summery of raw data:"),
                 verbatimTextOutput("raw_data_summary"),
-                h3("Correlations"),
-                plotOutput("raw_data_correlation")
+                h3("Correlations of raw data:"),
+                plotOutput("raw_data_correlation"),
+                h3("Summery of log data:"),
+                verbatimTextOutput("log_data_summary"),
+                h3("Correlations of log data:"),
+                plotOutput("log_data_correlation"),
+                h3("Data Table"),
+                tabsetPanel(
+                    tabPanel(
+                        'raw',
+                        DT::dataTableOutput("raw_table")
+                    ),
+                    tabPanel(
+                        'log',
+                        DT::dataTableOutput("log_table")
+                    )
+                ),
+                br()
             ),
-            tabPanel("Debug",
-                     verbatimTextOutput("debug"),)
-        ), )
+            tabPanel(
+                "Debug",
+                h3("raw"),
+                verbatimTextOutput("debug_raw"),
+                h3("log"),
+                verbatimTextOutput("debug_log"),
+            )
+        ),)
     )
 )
 
@@ -60,28 +88,23 @@ server <- function(input, output) {
     data(iris)
     
     ir.raw <- reactive({
-        iris[1:input$num_of_points, ]
-    })
-    
-    ir.raw_class <- reactive({
-        iris[1:input$num_of_points, 1:4]
-    })
-    
-    # log transform
-    ir.log_class <- reactive({
-        log(iris[1:input$num_of_points, 1:4])
-    })
-    
-    ir.species <- reactive({
-        iris[1:input$num_of_points, 5]
+        iris[1:input$num_of_points,]
     })
     
     ir.ca <- reactive({
-        CA(ir.raw_class(), graph = FALSE)    
+        CA(ir.raw()[1:4], graph = FALSE)
     })
     
-    cr <- reactive({
-        cor(ir.raw_class()[1:4])
+    # log transform
+    ir.log <- reactive({
+        df <- iris[1:input$num_of_points,]
+        df[, 1:4] <- log(iris[1:input$num_of_points, 1:4])
+        df
+    })
+    
+    # apply PCA - scale. = TRUE is highly advisable, but default is FALSE.
+    ir.pca <- reactive({
+        prcomp(ir.log()[, 1:4], center = TRUE, scale. = TRUE)
     })
     
     x_var <- reactive({
@@ -104,17 +127,16 @@ server <- function(input, output) {
         )
     })
     
-    output$pca_plot <- renderPlot({
-        # apply PCA - scale. = TRUE is highly advisable, but default is FALSE.
-        ir.pca <- prcomp(ir.log_class(), center = TRUE, scale. = TRUE)
+    output$pca_biplot <- renderPlot({
         g <-
             ggbiplot(
-                ir.pca,
+                ir.pca(),
                 choices = c(x_var(), y_var()),
                 obs.scale = 1,
                 var.scale = 1,
                 circle = TRUE,
-                groups = ir.species()
+                ellipse = TRUE,
+                groups = ir.raw()[, 5]
             )
         g <- g + scale_color_discrete(name = "")
         g <-
@@ -122,7 +144,12 @@ server <- function(input, output) {
                       legend.position = "top")
         print(g)
     })
-    output$ca_plot <- renderPlot({
+    
+    output$pca_summary <- renderPrint({
+        ir.pca()
+    })
+    
+    output$ca_biplot <- renderPlot({
         g <- fviz_ca_biplot(ir.ca(), repel = TRUE)
         print(g)
     })
@@ -130,13 +157,37 @@ server <- function(input, output) {
     output$raw_data_summary <- renderPrint({
         summary(ir.raw())
     })
-    output$raw_data_correlation <- renderPlot({
-        corrplot(cr(), method = "color")
+    
+    output$log_data_summary <- renderPrint({
+        summary(ir.log())
     })
     
-    output$debug <- renderPrint({
-        ir.raw_class()
+    output$raw_data_correlation <- renderPlot({
+        cr <- cor(ir.raw()[, 1:4])
+        corrplot(cr, method = "color")
     })
+    
+    output$log_data_correlation <- renderPlot({
+        cr <- cor(ir.log()[, 1:4])
+        corrplot(cr, method = "color")
+    })
+    
+    output$debug_raw <- renderPrint({
+        ir.raw()
+    })
+    
+    output$debug_log <- renderPrint({
+        ir.log()
+    })
+    
+    output$raw_table <- DT::renderDataTable({
+        DT::datatable(ir.raw(), options = list(lengthMenu = c(5, 30, 50), pageLength = 5))
+    })
+    
+    output$log_table <- DT::renderDataTable({
+        DT::datatable(ir.log(), options = list(lengthMenu = c(5, 30, 50), pageLength = 5))
+    })
+    
 }
 
 shinyApp(ui, server)
